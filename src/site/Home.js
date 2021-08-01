6109b359ec1127e7087fd6649e6a4462859d4371
Maxwell Horton <maxthomashorton@gmail.com>
2021-08-01 13:27:49 -0400

    First push. set up a none functional nav bar and put a test title on the home page.

---
+++ src/site/Home.js
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+
+const Home = () => {
+
+    return(
+        <>
+        <Navbar />
+        <h1>Hook Your Reader</h1>
+        </>
+    )
+}
+
+export default Home;
